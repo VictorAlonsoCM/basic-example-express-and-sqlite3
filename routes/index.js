63f6d9d9a8302bece374cc71103d60d2d566b82f
Victor Alonso <vacontrerasmeza@gmail.com>
2018-10-20 16:25:45 -0500

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
+// KNEX es para conectar a una base de datos y poder realizar migraciones
+// Es un tipo de ORM https://knexjs.org/
+// comando para inicializar un project con knex
+// knex init
+// crear una migración 
+// knex migrate:make usuarios
+// knex migrate:latest
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-
+var exec = require('child_process').exec;
+var os = require('os');
+var knex = require('knex');
+var knexFilen = require('../knexfile');
+var db = knex(knexFilen[process.env.NODE_ENV || 'development']);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,14 +31,66 @@ router.get('/formulario', function(req, res, next) {
 
 router.post('/nuevo', (req, res, next) => {
   const { nombre, edad } = req.body;
-  fs.writeFile('datos.json', JSON.stringify(req.body),(err) => {
-    if(!err){
-    res.send('Se ha guardado satisfactoriamente');
-    }else{
-      res.send('No se pudo guardar el usuario');
-    }
-  })
+
+  db('usuarios')
+    .insert({ nombre: nombre, edad: edad })
+    .catch(err => {
+      console.log(err);
+    }).then(data => {
+      res.redirect('/usuarios')
+    })
+
+  // fs.writeFile('datos.json', JSON.stringify(req.body),(err) => {
+  //   if(!err){
+  //   res.send('Se ha guardado satisfactoriamente');
+  //   }else{
+  //     res.send('No se pudo guardar el usuario');
+  //   }
+  // })
   //res.json(req.body);
+});
+
+router.get('/usuarios', (req, res, next) => {
+  db('usuarios')
+    .select('*')
+    .then(data => {
+      res.render('usuarios', {usuarios: data})
+    })
+});
+
+router.get('/user/:id/show', authentication, (req, res, next) => {
+  db('usuarios')
+    .select('*')
+    .where({ id: req.params.id })
+    .then(user => {
+      res.render('archivos/leer', user[0]);
+    })
+});
+
+function authentication(req, res, next){
+  if(req.params.id == 1){
+    next();
+  }else{
+    res.send('Usuario prohibido');
+  }
+}
+
+router.get('/comandos', (req, res, next) => {
+  console.log(os.platform());
+  // res.send(os.platform());
+
+  //const { cmd } = req.query;
+  // exec(cmd, (error, stdout, stderr) => {
+  //   console.log(stdout);
+  //   res.send(stdout);
+  // });
+  exec('ls', (error, stdout, stderr) => {
+    console.log(stdout);
+    res.send(stdout);
+  });
+  // exec('shutdown -r now', (error, stdout, stderr) => {
+  //   console.log(stdout);
+  // });
 });
 
 module.exports = router;
